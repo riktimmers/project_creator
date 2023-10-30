485cbb99e9f5aa4e94631bd9829f34bbd9228962
Rik <riktimmers@hotmail.com>
2023-10-30 21:21:18 +0100

    First version of a simple c++ project creator

---
+++ include/cmakelists.h
@@ -0,0 +1,32 @@
+#ifndef _H_CMAKE__ 
+#define _H_CMAKE__
+
+#include <string> 
+
+inline std::string createCmakeLists(const std::string project_name) {
+  return
+"project(" + project_name + ")\n"
+"cmake_minimum_required(VERSION 3.16.3)\n"
+"\n"
+"if(NOT CMAKE_BUILD_TYPE)\n"
+" set(CMAKE_BUILD_TYPE Release)\n"
+"endif()\n"
+"\n"
+"set(CMAKE_EXPORT_COMPILE_COMMANDS On)\n"
+"set(CMAKE_CXX_FLAGS_RELEASE '-O3')\n"
+"\n"
+"add_compile_options(-std=c++17 -Wall -Werror -Wextra -Wpedantic)\n"
+"\n"
+"include_directories(\n"
+"  include\n"
+")\n"
+"\n"
+"set(source_code\n"
+"  src/main.cpp\n"
+")\n"
+"\n"
+"add_executable(main ${source_code})\n"
+"\n"
+"target_link_libraries(main)\n";
+}
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <string> 
+#include <iostream>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fstream>
+#include "cmakelists.h"
+
+int main(int argc, char *argv[]) {
+  std::string project_name;
+
+  if (argc <= 1) {
+    std::cout << "Enter project name\n";
+    exit(0);
+  }
+
+  project_name = argv[1];
+
+  std::string path = "./" + project_name;
+  std::string build_path = "./" + project_name + "/build";
+  std::string src_path = "./" + project_name + "/src";
+  std::string include_path = "./" + project_name + "/include";
+
+  mkdir(path.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+  mkdir(build_path.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+  mkdir(src_path.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+  mkdir(include_path.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+  
+  std::string cmake_file = createCmakeLists(project_name);
+  std::ofstream output_file(path + "/CMakeLists.txt");
+
+  output_file << cmake_file;
+  output_file.close();
+}
