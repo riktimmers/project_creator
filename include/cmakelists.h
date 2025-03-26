49fe09fd8892f0ec8bca0aeeff3f5d5496df6c98
Rik <riktimmers@hotmail.com>
2025-03-26 20:08:31 +0100

    Update, when build debug, turn off -O3 optimization

--- include/cmakelists.h
+++ include/cmakelists.h
@@ -1,31 +1,34 @@
-#ifndef H_CMAKE__ 
+#ifndef H_CMAKE__
 #define H_CMAKE__
 
-#include <string> 
+#include <string>
 
 inline std::string createCmakeLists(const std::string project_name) {
-  return
-"cmake_minimum_required(VERSION 3.16.3)\n"
-"project(" + project_name + ")\n"
-"\n"
-"if(NOT CMAKE_BUILD_TYPE)\n"
-" set(CMAKE_BUILD_TYPE Release)\n"
-"endif()\n"
-"\n"
-"set(CMAKE_EXPORT_COMPILE_COMMANDS On)\n"
-"\n"
-"add_compile_options(-std=c++17 -O3 -Wall -Werror -Wextra -Wpedantic)\n"
-"\n"
-"include_directories(\n"
-"  include\n"
-")\n"
-"\n"
-"set(source_code\n"
-"  src/main.cpp\n"
-")\n"
-"\n"
-"add_executable(main ${source_code})\n"
-"\n"
-"target_link_libraries(main)\n";
+  return "cmake_minimum_required(VERSION 3.16.3)\n"
+         "project(" +
+         project_name +
+         ")\n"
+         "\n"
+         "if(NOT CMAKE_BUILD_TYPE)\n"
+         " set(CMAKE_BUILD_TYPE Release)\n"
+         " add_compile_options(-std=c++17 -O3 -Wall -Werror -Wextra -Wpedantic)\n"
+         "else()\n"
+         " add_compile_options(-std=c++17 -Wall -Werror -Wextra -Wpedantic)\n"
+         "endif()\n"
+         "\n"
+         "set(CMAKE_EXPORT_COMPILE_COMMANDS On)\n"
+         "\n"
+         "\n"
+         "include_directories(\n"
+         "  include\n"
+         ")\n"
+         "\n"
+         "set(source_code\n"
+         "  src/main.cpp\n"
+         ")\n"
+         "\n"
+         "add_executable(main ${source_code})\n"
+         "\n"
+         "target_link_libraries(main)\n";
 }
 #endif
